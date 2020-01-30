c0547d29b748e079ed1e3dc5657baee26ba2cd89
Andrew Jackson <andrewsyoga@gmail.com>
2020-01-30 15:23:26 +0000

    Add initial grid definition for spiral sort and a reference grid for checking x and y coordinate logic while defining tests and core functions

--- snailsort/test/snailsort/core_test.clj
+++ snailsort/test/snailsort/core_test.clj
@@ -1,6 +1,17 @@
 (ns snailsort.core-test
   (:require [clojure.test :refer :all]
-            [snailsort.core :refer :all]))
+            [snailsort.core :as snail]))
+
+; Reference coordinates
+;   x y
+; [[0 0] [0 1] [0 2]
+;  [1 0] [1 1] [1 2]
+;  [2 0] [2 1] [2 2]]
+
+(def grid
+  [[1 2 3]
+   [8 9 4]
+   [7 6 5]])
 
 (deftest a-test
   (testing "FIXME, I fail."
